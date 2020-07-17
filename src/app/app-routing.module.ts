cafab41b779c117b2df98b5ca9c1ebe60343ea07
Martin Braun <martinbraun@scoop.ch>
2020-07-17 16:23:22 +0200

    skeleton loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {SkeletonLoadingComponent} from './components/skeleton-loading/skeleton-loading.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: AppComponent },
+  { path: 'skeletonLoading',  component: SkeletonLoadingComponent },
+  { path: '**', component: AppComponent } // If no matching route found. Or you can navigate to the 404 error page.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
